01c3441bc869d2ffe42a7f74688f76221947942b
Keilly Giron <gironkeilly@gmail.com>
2022-03-17 22:26:44 -0600

    Avance del proyecto

---
+++ KidsToys/KidsToys.WebAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KidsToys.WebAdmin.MvcApplication" Language="C#" %>
---
+++ KidsToys/KidsToys.web/Controllers/ProductosController.cs
@@ -0,0 +1,21 @@
+﻿using KidsToys.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidsToys.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+
+            return View(listadeProductos);
+        }
+    }
+}
